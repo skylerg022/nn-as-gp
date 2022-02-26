1b9190ed30c8dcffb78260494a66ecdb57126622
Skyler Gray <skylerg022@gmail.com>
2022-02-26 09:47:00 -0700

    Removed gridsearch subdirectories in data file

--- HelperFunctions/Defaults.R
+++ HelperFunctions/Defaults.R
@@ -7,13 +7,9 @@ pic_units <- 'in'
 
 dirCheck <- function() {
   # if (!dir.exists('data')) dir.create('data')
-  if (!dir.exists('data/gridsearch_nn')) dir.create('data/gridsearch_nn')
-  if (!dir.exists('data/gridsearch_nn_trans')) dir.create('data/gridsearch_nn_trans')
-  if (!dir.exists('data/gridsearch_basis')) dir.create('data/gridsearch_basis')
   if (!dir.exists('pics')) dir.create('pics')
   return()
 }
-
 
 
 # Create basis/knot inputs ----------------------------------------------------
