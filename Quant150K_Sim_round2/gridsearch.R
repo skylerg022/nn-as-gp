f8f3f7a833eccd0574544d4f3edda53149870fe3
Skyler Gray <skylerg022@gmail.com>
2022-03-11 22:36:15 -0700

    EDA done

---
+++ Quant150K_Sim_round2/gridsearch.R
@@ -0,0 +1,36 @@
+## Gridsearch algorithm to tune neural network hyperparameters
+## for the simulated satellite data.
+
+## This is a gridsearch algorithm implemented for use through
+## either MacOS or some Linux OS. This implementation will not
+## run with parallelization on Windows OS
+
+library(tidyverse)
+library(keras)
+library(parallel)
+
+# Helper functions and data ------------------------------------------------------------
+
+# Set working directory if using RStudio
+if (rstudioapi::isAvailable()) {
+  setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+}
+
+# Make directories if needed
+source('../HelperFunctions/Defaults.R')
+dirCheck()
+source('../HelperFunctions/NNFunctions.R')
+source('../HelperFunctions/Preprocess.R')
+source('../HelperFunctions/Gridsearch.R')
+
+
+# Read in data
+load('data/dataset2_split.RData')
+
+n_cores <- 30
+gridsearch(input_type = 'nn', modeltype = 'custom', 
+           n_cores = n_cores, sqrt_n_knots = NULL,
+           loss = loss_mean_squared_error())
+gridsearch(input_type = 'nn', modeltype = 'lee2018', 
+           n_cores = n_cores, sqrt_n_knots = NULL,
+           loss = loss_mean_squared_error())
