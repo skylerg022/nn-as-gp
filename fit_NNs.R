c0fc5ff3f8e6f3d70aca8c82538019394de1c74f
Skyler Gray <skylerg022@gmail.com>
2022-05-03 16:49:48 -0600

    Major code changes to fit_NNs.R - Generalized code to run immediately after grid_analysis.R finishes running

--- fit_NNs.R
+++ fit_NNs.R
@@ -1,6 +1,11 @@
 
 # This code is meant to be run via the bash command line using
-# Rscript fit_NNs.R <dataset name>
+#  R CMD BATCH --no-save '--args <dataset name> <binary_data>' fit_NNs.R <logfile>.Rout &
+#  where <dataset name> is a string for the name of the directory holding the
+#  eda.R and data files of a given dataset and <binary_data> is either TRUE or
+#  FALSE to indicate whether the response data is binary or continuous.
+#  <logfile>.Rout is the file that will record output produced by the R file.
+#  The ampersand symbol (&) at the end will make this job run in the background.
 args <- commandArgs(trailingOnly = TRUE)
 
 # ...however, uncomment the following lines to run this code in RStudio
@@ -8,7 +13,7 @@ args <- commandArgs(trailingOnly = TRUE)
 # if (rstudioapi::isAvailable()) {
 #   setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 # }
-# args <- c('')
+# args <- c('Binary1Million', TRUE)
 
 # Check that dataset exists
 dirname <- paste0(getwd(), '/datasets/', args[1])
@@ -16,145 +21,104 @@ if (!dir.exists(dirname)) {
   message <- paste0('Directory ', dirname, ' does not exist.')
   stop(message)
 }
+setwd(dirname)
+
+# If second argument is FALSE, validation loss is MSE and must 
+#  be transformed to RMSE
+binary_data <- as.logical(args[2])
 
 source('../../functions/Defaults.R')
 source('../../functions/Preprocess.R')
 source('../../functions/NNFunctions.R')
 
-# Read in data
+# Read in data & model settings
 load('data/DataSplit.RData')
-
-
-# Model Fitting and Potential Picture Generation ------------------------------------
-
-pred <- cbind(x_test,
-              latlong_nn_custom = 0, latlong_nn_lee = 0,
-              latlong_trans_custom = 0, latlong_trans_lee = 0,
-              basis_4by4_custom = 0, basis_4by4_lee = 0,
-              basis_4by4_20by20_custom = 0, basis_4by4_20by20_lee = 0)
 n_train <- nrow(x_train) + nrow(x_val)
 
-# Lat-Long NN
-model_pars_lee <- c(n_layers = 8, layer_width = 2^8, 
-                    learning_rate = 0.0001192127,
-                    weight_decay = 9.380247e-08, 
-                    epochs = 61, batch_size = 2^5, 
-                    sigma_w = 1.121034, sigma_b = 0.32748548, 
-                    model_num = 100)
-model_pars <- c(n_layers = 8, layer_width = 2^8,
-                epochs = 45, batch_size = 2^8,
-                decay_rate = 0.1 / (n_train %/% 2^8), dropout_rate = 0,
-                model_num = 235)
-
-## Lee2018
-pred[,'latlong_nn_lee'] <- 
-  evalNetwork(x_train, y_train, x_val, y_val,
-              model_pars = model_pars_lee, 
-              x_test = x_test, y_test = NULL,
-              type = 'nn', pars_type = 'lee2018',
-              sqrt_n_knots = NULL,
-              plot = FALSE)$yhat_test
-## Custom
-fit <- evalNetwork(x_train, y_train, x_val, y_val,
-                   model_pars = model_pars, 
-                   x_test = x_test, y_test = y_test,
-                   binary_data = TRUE,
-                   type = 'nn', pars_type = 'custom',
-                   sqrt_n_knots = NULL,
-                   plot = FALSE)
-pred[,'latlong_nn_custom'] <- fit$yhat_test
+models <- read_csv('data/final_models_setup.csv')
+# Only keep 4 of the 8 models for fitting:
+#   The raw input types and the best non-identity
+#   basis function expansion input type 
+#   (transformed, radial basis, or multi-resolution basis)
+final_models <- models %>%
+  filter(type == 'nn') %>%
+  bind_rows(models %>%
+              filter(type != 'nn') %>%
+              group_by(pars_type) %>%
+              filter(min_loss == min(min_loss)))
 
 
-# Lat-Long Transformed NN
-model_pars_lee <- c(n_layers = 4, layer_width = 2^9, 
-                    learning_rate = 0.0009401810,
-                    weight_decay = 2.496104e-07, 
-                    epochs = 55, batch_size = 2^8, 
-                    sigma_w = 1.2427385, sigma_b = 0.06185929, 
-                    model_num = 7)
-model_pars <- c(n_layers = 8, layer_width = 2^7,
-                epochs = 60, batch_size = 2^8,
-                decay_rate = 0.1 / (n_train %/% 2^8), dropout_rate = 0,
-                model_num = 230)
+# Create prediction plots in pics/final/ and return model performance metrics
+n_models <- nrow(final_models)
+model_metrics <- matrix(NA, nrow = n_models, ncol = 10)
+for (i in 1:n_models) {
+# model_metrics <- apply(final_models, 1, function(curmodel) {
+  curmodel <- final_models[i,]
+  # Raise epochs to nearest multiple of 5
+  remainder <- curmodel$min_loss_epoch %% 5
+  total_epochs <- ifelse(remainder > 0, curmodel$min_loss_epoch + (5 - remainder),
+                         curmodel$min_loss_epoch)
+  # Roughly decay rate per epoch
+  new_decay <- ifelse(curmodel$decay_rate > 0,
+                      0.1 / (n_train %/% curmodel$batch_size), 0)
+  
+  # Prep hyperparameters for model fitting
+  if (curmodel$pars_type == 'custom') {
+    model_pars <- c(n_layers = curmodel$n_layers, 
+                    layer_width = curmodel$layer_width,
+                    epochs = total_epochs, 
+                    batch_size = curmodel$batch_size,
+                    decay_rate = new_decay, 
+                    dropout_rate = curmodel$dropout_rate,
+                    model_num = curmodel$model_num)
+  } else if (curmodel$pars_type == 'lee2018') {
+    model_pars <- c(n_layers = curmodel$n_layers, 
+                    layer_width = curmodel$layer_width, 
+                    learning_rate = curmodel$learning_rate,
+                    weight_decay = curmodel$weight_decay, 
+                    epochs = total_epochs, 
+                    batch_size = curmodel$batch_size, 
+                    sigma_w = curmodel$sigma_w, 
+                    sigma_b = curmodel$sigma_b, 
+                    model_num = curmodel$model_num)
+  }
+  
+  # Set hyperparameters for input data
+  if (curmodel$type %in% c('nn', 'nn_trans')) {
+    type <- curmodel$type
+    sqrt_n_knots <- NULL
+  } else if (curmodel$type == 'basis_4by4') {
+    type <- 'basis'
+    sqrt_n_knots <- c(4)
+  } else if (curmodel$type == 'basis_4by4_20by20') {
+    type <- 'basis'
+    sqrt_n_knots <- c(4, 20)
+  }
+  
+  # Fit NN
+  fit <- evalNetwork(x_train, y_train, x_val, y_val,
+                     model_pars = model_pars, 
+                     x_test = x_test, y_test = y_test,
+                     binary_data = binary_data,
+                     type = type, 
+                     pars_type = curmodel$pars_type,
+                     sqrt_n_knots = sqrt_n_knots)
+  
+  train_mets <- unlist(fit$metrics$train)
+  names(train_mets) <- paste0('train_', names(train_mets))
+  test_mets <- unlist(fit$metrics$test)
+  names(test_mets) <- paste0('test_', names(test_mets))
+  
+  mets <- c(train_mets, test_mets)
+  model_metrics[i,] <- mets
+}
 
-## Lee2018
-pred[,'latlong_trans_lee'] <- 
-  evalNetwork(x_train, y_train, x_val, y_val,
-              model_pars = model_pars_lee, 
-              x_test = x_test, y_test = NULL,
-              type = 'nn_trans', pars_type = 'lee2018',
-              sqrt_n_knots = NULL,
-              plot = FALSE)$yhat_test
-## Custom
-pred[,'latlong_trans_custom'] <- 
-  evalNetwork(x_train, y_train, x_val, y_val,
-              model_pars = model_pars, 
-              x_test = x_test, y_test = NULL,
-              type = 'nn_trans', pars_type = 'custom',
-              sqrt_n_knots = NULL,
-              plot = FALSE)$yhat_test
+# Give metric names to columns
+colnames(model_metrics) <- names(mets)
 
+# Add model performance to model
+final_performance <- final_models %>%
+  bind_cols(as.data.frame(model_metrics))
 
-# Basis 4by4
-model_pars_lee <- c(n_layers = 8, layer_width = 2^8, 
-                    learning_rate = 0.0024747043,
-                    weight_decay = 2.906369e-08, 
-                    epochs = 50, batch_size = 2^8, 
-                    sigma_w = 2.041133, sigma_b = 0.643556342, 
-                    model_num = 90)
-model_pars <- c(n_layers = 4, layer_width = 2^9,
-                epochs = 95, batch_size = 2^7,
-                decay_rate = 0.1 / (n_train %/% 2^7), dropout_rate = 0,
-                model_num = 214)
-
-## Lee2018
-pred[,'basis_4by4_lee'] <- 
-  evalNetwork(x_train, y_train, x_val, y_val,
-              model_pars = model_pars_lee, 
-              x_test = x_test, y_test = NULL,
-              type = 'basis', pars_type = 'lee2018',
-              sqrt_n_knots = c(4),
-              plot = FALSE)$yhat_test
-## Custom
-pred[,'basis_4by4_custom'] <- 
-  evalNetwork(x_train, y_train, x_val, y_val,
-              model_pars = model_pars, 
-              x_test = x_test, y_test = NULL,
-              type = 'basis', pars_type = 'custom',
-              sqrt_n_knots = c(4),
-              plot = FALSE)$yhat_test
-
-
-# Basis 4by4&20by20
-model_pars_lee <- c(n_layers = 4, layer_width = 2^7,
-                    learning_rate = 0.0025565280,
-                    weight_decay = 2.992099e-08,
-                    epochs = 57, batch_size = 2^7,
-                    sigma_w = 0.8994819, sigma_b = 0.53524871,
-                    model_num = 405)
-model_pars <- c(n_layers = 4, layer_width = 2^9,
-                epochs = 85, batch_size = 2^8,
-                decay_rate = 0.1 / (n_train %/% 2^8), dropout_rate = 0,
-                model_num = 238)
-
-## Lee2018
-pred[,'basis_4by4_20by20_lee'] <-
-  evalNetwork(x_train, y_train, x_val, y_val,
-              model_pars = model_pars_lee,
-              x_test = x_test, y_test = NULL,
-              type = 'basis', pars_type = 'lee2018',
-              sqrt_n_knots = c(4, 20),
-              plot = FALSE)$yhat_test
-## Custom
-pred[,'basis_4by4_20by20_custom'] <- 
-  evalNetwork(x_train, y_train, x_val, y_val,
-              model_pars = model_pars, 
-              x_test = x_test, y_test = NULL,
-              type = 'basis', pars_type = 'custom',
-              sqrt_n_knots = c(4, 20),
-              plot = FALSE)$yhat_test
-
-# Save prediction results
-pred %>%
-  as.data.frame() %>%
-  write_csv('data/dataset2_testpreds.csv')
+# Save final model performance metrics
+write_csv(final_performance, 'data/final4_models_performance.csv')
